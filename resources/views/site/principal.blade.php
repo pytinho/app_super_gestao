339acfb353784d8d6ec1692186f5284c3dbd6853
Dayana Alencar <dayanaalencar.fc@gmail.com>
2025-02-13 14:30:14 -0300

    lfg

---
+++ resources/views/site/principal.blade.php
@@ -0,0 +1,15 @@
+<h3> Principal</h3>
+
+<ul>
+    <li>
+        <a href="/">Principal</a>
+    </li>
+    
+    <li>
+        <a href="/sobrenos">Sobre nós</a>
+    </li>
+
+    <li>
+        <a href="/contato">Contato</a>
+    </li>
+</ul>
---
+++ resources/views/site/sobrenos.blade.php
@@ -0,0 +1,15 @@
+<h3> Sobre Nós (view)</h3>
+
+<ul>
+    <li>
+        <a href="/">Principal</a>
+    </li>
+    
+    <li>
+        <a href="/sobrenos">Sobre nós</a>
+    </li>
+
+    <li>
+        <a href="/contato">Contato</a>
+    </li>
+</ul>
